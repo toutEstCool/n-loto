4addebaa47f821439ee45a0e2eac38edae4b02ce
toutEstCool <danilapanarin96@gmail.com>
2025-02-04 18:51:37 +0600

    added: sheet

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
+'use client'
+
+import { Sheet } from "@/shared/components/sheet/sheet";
+import { useState } from "react";
 
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggler = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <>
       <h1>Hello</h1>
+      <Sheet />
     </>
   );
 }
