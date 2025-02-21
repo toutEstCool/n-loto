0a07c81c1ff704d8d8df10156e4cbeb5d2ea2fa0
ImanArs <imanbaevemaill.ru@gmail.com>
2025-02-21 15:55:19 +0600

    add adaptive charity

--- app/promo-charity/page.tsx
+++ app/promo-charity/page.tsx
@@ -11,21 +11,36 @@ const PromoCharity = async () => {
     <div className="mt-[70px] sm:mt-[88px] md:mt-[116px] min-h-screen relative mb-16 xl:mb-0">
       <Container>
         <div
-          className="bg-[url(/images/promo_charity-bg.jpg)] h-[320px] bg-contain bg-center flex flex-col text-white
+          className="bg-[url(/images/promo_charity-bg.jpg)] h-[320px] bg-cover bg-center flex flex-col text-white
           pt-10 pb-8 pl-5 rounded-[16px]
         
         "
         >
           <div className="max-w-[470px] h-full   flex flex-col justify-between">
-            <h1 className="text-[32px] leading-8 font-Acrom font-bold">
+            <h1
+              className="font-Acrom font-bold
+              text-[32px] leading-8 w-[294px]
+              md:text-[32px] md:leading-8 md:w-full
+            "
+            >
               ИГРАЯ В ЛОТЕРЕИ, <br />
               ВЫ ПОМОГАЕТЕ ЛЮДЯМ
             </h1>
-            <p className="mb-2 text-[16px] leading-5 font-HavalMittel-regular">
+            <p
+              className="mb-2 font-HavalMittel-regular
+              text-[14px] leading-4
+              xl:text-[16px] xl:leading-5
+            "
+            >
               Часть денег с проданных билетов идёт на благотворительные проекты
               по всей России.
             </p>
-            <button className="max-w-[160px] text-[#2f41b0] text-[20px] leading-6 font-HavalMittel-medium border-white border px-5 py-3 bg-white rounded-[12px]">
+            <button
+              className="max-w-[160px] text-[#2f41b0] font-HavalMittel-medium border-white border bg-white rounded-[12px]
+              p-2 text-[20px] leading-6
+              md:px-5 md:py-3
+            "
+            >
               Хочу помочь
             </button>
           </div>
--- shared/components/promo-charity-card/ui/promo-charity-card.tsx
+++ shared/components/promo-charity-card/ui/promo-charity-card.tsx
@@ -24,14 +24,27 @@ export const PromoCharityCard = ({ charity }: Props) => {
     <div className="bg-white p-8 rounded-[20px] flex flex-col gap-5">
       <div className="flex w-full justify-between">
         <div className="max-w-[780px] h-full">
-          <h3 className="text-[22px] leading-6 font-HavalMittel-bold text-[#364059]">
+          <p
+            className="text-[#5e6976] md:hidden
+            text-[18px] leading-6 font-HavalMittel-medium
+          "
+          >
+            {formatDate(charity.date)}
+          </p>
+          <h3
+            className="font-HavalMittel-bold text-[#364059]
+            text-[18px] leading-6
+            xl:text-[22px]
+          "
+          >
             {charity.title}
           </h3>
 
           <div
-            className={`mt-5 flex flex-col gap-[18px] text-[16px] leading-5 font-HavalMittel-regular text-[#5e6976] ${
-              isExpanded ? '' : 'max-h-[145px] overflow-hidden'
-            }`}
+            className={`md:mt-5 flex flex-col gap-[18px] font-HavalMittel-regular text-[#5e6976]
+                text-[14px] leading-5
+                xl:text-[16px]
+              ${isExpanded ? '' : 'max-h-[145px] overflow-hidden'}`}
           >
             {charity.description.map((text, index) => (
               <p key={index}>{text}</p>
@@ -40,7 +53,11 @@ export const PromoCharityCard = ({ charity }: Props) => {
 
           {!isExpanded && (
             <button
-              className="mt-3 text-[#024cf8] underline text-[18px] font-HavalMittel-bold"
+              className="mt-3 text-[#024cf8] underline font-HavalMittel-bold
+                text-[12px]
+                md:text-[16px]
+                xl:text-[18px]
+              "
               onClick={() => setIsExpanded(true)}
             >
               Показать весь текст
@@ -49,10 +66,15 @@ export const PromoCharityCard = ({ charity }: Props) => {
         </div>
 
         <div className="h-auto flex flex-col justify-between max-w-[314px]">
-          <p className="text-[#5e6976] text-[20px] leading-5 font-HavalMittel-bold text-end">
+          <p
+            className="text-[#5e6976] text-end hidden
+            md:text-[18px] md:leading-6 md:min-w-[150px] md:font-HavalMittel-medium md:block
+            xl:text-[20px] xl:leading-5 xl:font-HavalMittel-bold 
+          "
+          >
             {formatDate(charity.date)}
           </p>
-          <div className="w-[314px] p-3 rounded-[12px] bg-[#efebe0] flex flex-col gap-3">
+          <div className="hidden w-[314px] p-3 rounded-[12px] bg-[#efebe0] xl:flex flex-col gap-3">
             <div className="flex items-center gap-2">
               <Image
                 src={charity.companyLogo}
@@ -77,8 +99,32 @@ export const PromoCharityCard = ({ charity }: Props) => {
         alt="promo_charity-card"
         width={1000}
         height={1000}
+        className="min-h-[180px] md:min-h-[360px] rounded-[12px]"
         style={{ width: '100%', height: '100%' }}
       />
+      <div
+        className="pt-6  border-t-[3px] max-w-[340px] flex items-center justify-between
+        md:border-0 md:rounded-[16px] md:bg-[#efebe0] md:p-2
+        xl:hidden
+      "
+      >
+        <Image
+          src={charity.companyLogo}
+          alt="promo_charity-card"
+          width={40}
+          height={40}
+          className="rounded-[12px]"
+        />
+        <p className="text-[#5e6976] text-[20px] leading-5 font-HavalMittel-bold">
+          {charity.companyName}
+        </p>
+        <button className="md:hidden border-2 border-[#2f41b0] rounded-[12px] px-5 py-3 text-[#2f41b0] text-[18px] leading-6 font-HavalMittel-medium">
+          Купить
+        </button>
+        <button className="hidden md:block md:h-[32px] border-2 border-[#2f41b0] rounded-[12px] p-1 text-[#2f41b0] text-[18px] leading-6 font-HavalMittel-medium">
+          Купить • {charity.ticketPrice} ₽
+        </button>
+      </div>
     </div>
   );
 };
